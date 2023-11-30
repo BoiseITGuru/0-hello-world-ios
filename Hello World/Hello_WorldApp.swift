8ca4d8b892632b9dc2a869e94ab662e29b569772
BoiseITGuru <dev@boiseitguru.com>
2023-11-30 14:57:08 -0700

    redesign using FlowComponents and ecDAO libraries

--- Hello World/Hello_WorldApp.swift
+++ Hello World/Hello_WorldApp.swift
@@ -6,17 +6,43 @@
 //
 
 import SwiftUI
+import FCL
+import Flow
+import ecDAO
 
 @main
 struct Hello_WorldApp: App {
+    let testAccount = "YOUR_TESTNET_ACCOUNT"
+    let walletConnectID = "YOUR_WALLETCONNECT_PROJECT_ID"
+    @State private var title = "Hello World"
+    @State private var description = "Read and write from the Flow Blockchain for the first time!"
     
     init() {
-        FlowManager.shared.setup()
+        let defaultProvider: FCL.Provider = .devWallet
+        let defaultNetwork: Flow.ChainID = .emulator
+        let accountProof = FCL.Metadata.AccountProofConfig(appIdentifier: "Hello World")
+        let walletConnect = FCL.Metadata.WalletConnectConfig(urlScheme: "helloWorld://", projectID: walletConnectID)
+        let metadata = FCL.Metadata(appName: "Hello World",
+                                    appDescription: "Hello Word Demo App for Emerald Academy",
+                                    appIcon: URL(string: "https://academy.ecdao.org/ea-logo.png")!,
+                                    location: URL(string: "https://academy.ecdao.org/")!,
+                                    accountProof: accountProof, 
+                                    walletConnectConfig: walletConnect)
+        fcl.config(metadata: metadata,
+                   env: defaultNetwork,
+                   provider: defaultProvider)
+
+        fcl.config
+            .put("0xDeployer", value: fcl.currentEnv == .emulator ? "0xf8d6e0586b0a20c7" : testAccount)
+        
+        ecDAOinit()
     }
     
     var body: some Scene {
         WindowGroup {
-            RouterView()
+            RouterView(title: $title, desc: $description) {
+                HelloWorldView()
+            }
         }
     }
 }
--- Hello World/Views/HelloWorldView.swift
+++ Hello World/Views/HelloWorldView.swift
@@ -8,79 +8,110 @@
 import SwiftUI
 import FCL
 import Flow
+import FlowComponents
+import ecDAO
 
 struct HelloWorldView: View {
+    @State var showCodeSheet: Bool = false
+    @StateObject var codeConfig: CodeViewConfig = CodeViewConfig(title: "Title", description: "Description", swiftCode: Scripts.getGreeting, cadenceCode: Transactions.changeGreeting)
     @State var greetingDisplay = ""
+    @State var showGreetingAlert = false
     @State var greetingText = ""
     
     var body: some View {
         VStack {
-            HStack(spacing: 6) {
-                Image("ea-logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 40)
-                Text("Emerald Academy")
-                    .font(.title)
-                    .foregroundStyle(Color.defualtAccentColor)
-            }
+            AcademyHeader()
             
-            ButtonView(title: "Get Greeting", action: { Task { await getGreeting() } })
-                .padding(.bottom, 4)
+            getGreetingView
             
-            Text(greetingDisplay)
-                .font(.title)
-                .frame(maxWidth: .infinity, maxHeight: 200)
-                .foregroundStyle(Color.white)
-                .background(Color.secondaryAccentColor)
-                .cornerRadius(15)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(Color.defualtAccentColor, lineWidth: 3)
-                )
-                .padding(.bottom, 20)
-            
-            TextField("Change Greeting", text: $greetingText)
-                .submitLabel(.send)
-                .foregroundStyle(Color.white)
-                .onSubmit {
-                    guard greetingText.isEmpty == false else { return }
-                    Task { await changeGreeting() }
-                }
-                .frame(maxWidth: .infinity, maxHeight: 50)
-                .background(Color.secondaryAccentColor)
-                .cornerRadius(15)
-                .padding(3)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(Color.defualtAccentColor, lineWidth: 3)
-                )
-                .padding(.bottom, 4)
-                .onReceive(FlowManager.shared.$errorTx) { txId in
-                    if (txId != nil) {
-                        greetingDisplay = "Error with Tx: \(txId)"
-                    }
-                }
-            
-            ButtonView(title: "Change Greeting") {
-                guard greetingText.isEmpty == false else { return }
-                Task { await changeGreeting() }
-            }
+            changeGreetingView
             
             Spacer()
-            
-            ButtonView(title: "Sign Out", action: { Task { try? await fcl.unauthenticate() } })
+        }
+        .sheet(isPresented: $showCodeSheet, onDismiss: { codeConfig.codeType = .swift }) {
+            CodeSheet(codeType: $codeConfig.codeType, title: $codeConfig.title, description: $codeConfig.description, swiftCode: $codeConfig.swiftCode, cadenceCode: $codeConfig.cadenceCode)
+        }
+    }
+    
+    var getGreetingView: some View {
+        GroupBox(label: Text("1. Read Your Greeting")) {
+            VStack {
+                Text("Run a script to read your greeting from the Flow Blockchain.")
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical)
+
+                
+                Button(action: {
+                    updateCodeConfig(title: "getGreeting Script", description: "This is the FCL code that runs a script to read your greeting from the Flow Blockchain.", swiftCode: SwiftCode.getGreeting, cadenceCode: Scripts.getGreeting)
+                }, label: {
+                    HStack(spacing: 0) {
+                        Image(systemName: "chevron.left.slash.chevron.right")
+                            .font(.body)
+                        Text("View Script")
+                    }
+                })
+                .foregroundStyle(Color.eaPrimary)
+                .buttonStyle(PlainButtonStyle())
+                .padding(.bottom, 3)
+                
+                ButtonView(title: "Get Greeting") {
+                    Task {
+                        await getGreeting()
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 40)
+                .alert("Current Greeting", isPresented: $showGreetingAlert) {
+                    Button("OK", role: .cancel) {}
+                } message: {
+                    Text(greetingDisplay)
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+        }
+    }
+    
+    var changeGreetingView: some View {
+        GroupBox(label: Text("2. Change Your Greeting")) {
+            VStack {
+                Text("Insert a new greeting in the TextField below, then execute the transaction to save your new message on the Flow Blockchain.")
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical)
+                
+                TextField("Greeting Text", text: $greetingText)
+                    .padding(3)
+                    .border(Color.eaPrimary)
+                    .padding(.bottom, 10)
+
+                
+                Button(action: {
+                    updateCodeConfig(title: "changeGreeting Transaction", description: "This is the FCL code that runs a transaction to change your greeting on the Flow Blockchain.", swiftCode: SwiftCode.changeGreeting, cadenceCode: Transactions.changeGreeting)
+                }, label: {
+                    HStack(spacing: 0) {
+                        Image(systemName: "chevron.left.slash.chevron.right")
+                            .font(.body)
+                        Text("View Transaction")
+                    }
+                })
+                .foregroundStyle(Color.eaPrimary)
+                .buttonStyle(PlainButtonStyle())
+                .padding(.bottom, 3)
+                
+                ButtonView(title: "Change Greeting") {
+                    Task {
+                        await changeGreeting()
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 40)
+            }
         }
     }
     
     func getGreeting() async {
-        await MainActor.run {
-            greetingDisplay = ""
-        }
         do {
             let block = try await fcl.query {
                 cadence {
-                    Scripts.getGreeting.rawValue
+                    Scripts.getGreeting.code
                 }
 
                 gasLimit {
@@ -88,28 +119,34 @@ struct HelloWorldView: View {
                 }
             }.decode(String.self)
             await MainActor.run {
-                greetingDisplay = block 
+                greetingDisplay = block
+                showGreetingAlert.toggle()
             }
         } catch {
             // TODO: Improve Error Handling
-            await MainActor.run {
-                greetingDisplay = error.localizedDescription
-            }
+            print(error)
         }
     }
     
     func changeGreeting() async {
         do {
-            let txId = try await fcl.mutate(cadence: Transactions.changeGreeting.rawValue, args: [.string(greetingText)])
+            let txId = try await fcl.mutate(cadence: Transactions.changeGreeting.code, args: [.string(greetingText)])
             await MainActor.run {
                 self.greetingText = ""
             }
-            FlowManager.shared.subscribeTransaction(txId: txId)
+            flowManager.subscribeTransaction(txId: txId)
         } catch {
             // TODO: Improve Error Handling
-            await MainActor.run {
-                greetingDisplay = error.localizedDescription
-            }
+            print(error)
         }
+    }
+    
+    private func updateCodeConfig(title: String, description: String, swiftCode: CadenceCode, cadenceCode: CadenceCode) {
+        codeConfig.title = title
+        codeConfig.description = description
+        codeConfig.swiftCode = swiftCode
+        codeConfig.cadenceCode = cadenceCode
+            
+        self.showCodeSheet = true
     }
 }
